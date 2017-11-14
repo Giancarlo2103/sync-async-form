606f982808f533f1c0c432dd9bdfc6a3faa203c6
Alumno13 J601 <a13j601@campustls.local>
2017-11-14 12:11:27 -0500

    adssad

--- public/main.js
+++ public/main.js
@@ -9,21 +9,37 @@ $(document).ready(function (){
 
         event.preventDefault();
 
+        /*$.ajax('find.html',{
+            method = 'post',
+            data : {
+                'first_name' : first_name
+            }
+        });*/
+            
 
         var first_name =$('#first_name').val();
+        var last_name =$('#last_name').val();
+        var gender =$('input[name="gender"]:checked').val(); /*esto me devuelve un array de coinsidencia*/
+        var interest =$('input[name="interest"]:checked'); /*checked (selector)*/
+        var interests =  new Array();           
+                     
+        for (var i = 0; i < interest.length; i ++) {
+            var temp_interest = interest[i];
+            interests.push(temp_interest.value);
+        }
+            
+        var payload =  {
+            'first_name' :first_name,
+            'last_name' :last_name,
+            'gender' : gender,
+            'interests' :interests,
+            
+        }
+        $.post('find.html',payload);
 
-        /*$.ajax('find.html',{
-        	method = 'post',
-        	data : {
-        		'first_name' : first_name
-        	}
-        });*/
+       
 
+        
+        });
 
-        $.post('find.html',{'first_name' : first_name});
-         $.get('find.html',{'first_name' : first_name});
-
-    });
-
-
-})
+});
