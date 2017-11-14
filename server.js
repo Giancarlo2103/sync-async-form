016aca3c18dc23288241dee25b413db1dc2578f3
Alumno13 J601 <a13j601@campustls.local>
2017-11-14 12:55:10 -0500

    asdasd

--- server.js
+++ server.js
@@ -5,25 +5,25 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+ extended: true
 }));
 
 app.use("/", express.static("public"));
 
-app.get("/find", (request, response) => {
-    console.log(request.query.first_name)
-    console.log(request.query.last_name);
-     response.json({"method": "get", "success": true});
-    
+app.post("/find", (request, response) => {
+   console.log(request.body.first_name);
+   console.log(request.body.last_name);
+   response.json({"method": "post", "success": true});
+   
 });
 
-app.post("/find", (request, response) => {
-    console.log(request.query.first_name)
-    console.log(request.query.last_name);
-     response.json({"method": "post", "success": true});
-    
+app.get("/find", (request, response) => {
+   console.log(request.query.first_name);
+   console.log(request.query.last_name);
+   response.json({"method": "get", "success": true});
+   
 });
 
 app.listen(8080, function () {
-  console.log('Listening on port 8080!')
+ console.log('Listening on port 8080!')
 });
